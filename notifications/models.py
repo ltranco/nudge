46859e73b968c634e0c5a3627bd49d7e9138cc18
Long Tran <long_tran@apple.com>
2019-08-30 15:50:22 -0700

    update model

--- notifications/models.py
+++ notifications/models.py
@@ -4,6 +4,6 @@ from django.db import models
 
 class Topic(models.Model):
     owner = models.ForeignKey(get_user_model(), blank=False, null=False, on_delete=models.CASCADE)
-    name = models.TextField(blank=False)
-    url = models.URLField()
+    name = models.CharField(blank=False, null=False, max_length=9999)
+    url = models.CharField(blank=True, null=True, max_length=9999)
     is_completed = models.BooleanField(default=False)
