a07671c78356207f79df9c9d0fff94513472164b
Long Tran <long_tran@apple.com>
2019-08-31 13:01:34 -0700

    str for topic model

--- notifications/models.py
+++ notifications/models.py
@@ -7,3 +7,6 @@ class Topic(models.Model):
     name = models.CharField(blank=False, null=False, max_length=9999)
     url = models.CharField(blank=True, null=True, max_length=9999)
     is_completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return '{} {}'.format(self.name, self.owner)
